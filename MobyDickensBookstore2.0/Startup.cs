ca4d87b7dfb0ec91c8e1975546607e9b4a173afe
Andrew-Garofalo <agarofalo1@gmail.com>
2019-04-13 12:41:56 -0400

    Add project files.

---
+++ MobyDickensBookstore2.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MobyDickensBookstore2._0.Startup))]
+namespace MobyDickensBookstore2._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
